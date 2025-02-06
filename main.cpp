77c229c2871305b5e94ee6ef720a5bd993ed26d2
Abdumo�Min Abdumalikov <a.abdumalikov@172-10-61-43.lightspeed.moblal.sbcglobal.net>
2025-02-06 10:49:22 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //problem 1
+    // int a;
+    // cin >> a;
+    // if (a>0) {
+    //     cout<<"Positive number"<<endl;
+    // }
+    // else if (a=0) {
+    //     cout<<"0"<<endl;
+    // }
+    // else {
+    //     cout<<"Negative number"<<endl;
+    // }
+    // return 0;
+    //problem 2
+    // int a;
+    // cin>>a;
+    // if (a%2==0) {
+    //     cout<<"even number"<<endl;
+    // }
+    // else {
+    //     cout<<"odd number"<<endl;
+    //}
+    //problem 3
+    // int a, b;
+    // cin>>a>>b;
+    // if(a>b) {
+    //     cout<<"Largest number is "<<a<<endl;
+    // } else {
+    //     cout<<"Largest number is "<<b<<endl;
+    // }
+    //problem 4
+    // int a,b,c;
+    // cin>>a>>b>>c;
+    // if (a>b && a>c) {
+    //     cout<<"Largest number is "<<a<<endl;
+    // }
+    // else if (b>a && b>c) {
+    //     cout<<"Largest number is "<<b<<endl;
+    // }
+    // else {
+    //     cout<<"Largest number is "<<c<<endl;
+    // }
+    // return 0;
+    //problem 5
+    // int year;
+    // cin >> year;
+    // if (year%4==0) {
+    //     cout<<"Leap year"<<endl;
+    // }
+    // else {
+    //     cout<<"Not a Leap year"<<endl;
+    // }
+    // return 0;
+    //problem 6
+    // int a;
+    // cin >> a;
+    // if (a<20) {
+    //     cout << "too slow" << endl;
+    // }
+    // else if (a>80) {
+    //     cout << "too fast" << endl;
+    // }
+    // else {
+    //     cout << "Normal" << endl;
+    // }
+    // return 0;
+    //problem 7
+    // int m;
+    // cin >> m;
+    // if (m>=50) {
+    //     cout<<"Pass"<<endl;
+    // }
+    // else {
+    //     cout<<"Fail"<<endl;
+    // }
+    // return 0;
+    //problem 8
+    // int a;
+    // cin >> a;
+    // if (a>0) {
+    //     cout<<"Positive"<<endl;
+    // }
+    // else if (a==0) {
+    //     cout<<"Zero"<<endl;
+    // }
+    // else {
+    //     cout<<"Negative"<<endl;
+    // }
+    // return 0;
+    //problem 9
+    // char c;
+    // cin>>c;
+    // if (c=='g') {
+    //     cout<<"Go!"<<endl;
+    // }
+    // else if (c=='y') {
+    //     cout<<"Get ready!"<<endl;
+    // }
+    // else if (c=='r') {
+    //     cout<<"Stop"<<endl;
+    // }
+    // return 0;
+    //problem 10
+    // int a;
+    // cin >> a;
+    // int b;
+    // b=a/10;
+    // if (a==100) {
+    //     cout<<"A";
+    // }
+    // switch(b) {
+    //     case 9:
+    //         cout<<"A";
+    //     break;
+    //     case 8:
+    //         cout<<"B";
+    //     break;
+    //     case 7:
+    //         cout<<"C";
+    //     break;
+    //     case 6:
+    //         cout<<"D";
+    //     break;
+    //     default:
+    //         cout<<"F";
+    //
+    // }
+    //problem 11
+    // int a,b;
+    // cin>>a>>b;
+    // if (a%b==0) {
+    //     cout<<a<<" is divisible by "<<b<<endl;
+    // }
+    // else {
+    //     cout<<a<<" is not divisible by "<<b<<endl;
+    // }
+    // return 0;
+    //problem 12
+    // int a,b,c;
+    // cin>>a>>b>>c;
+    // if (a+b>c && a+c>b && b+c>a) {
+    //     cout<<"Triangle can be formed"<<endl;
+    // }
+    // else {
+    //     cout<<"Triangle is not valid"<<endl;
+    // }
+    // return 0;
+    //problem 13
+    // char c;
+    // cin>>c;
+    // if(c>='a' && c<='z') {
+    //     cout<<"Lowercase"<<c<<endl;
+    // }
+    // else if(c>='A' && c<='Z') {
+    //     cout<<"Uppercase alphabet"<<endl;
+    // }
+    // else {
+    //     cout<<"not an Alphabet"<<endl;
+    // }
+    // return 0;
+    //problem 14
+    // int w;
+    // float f;
+    // cout<<"Enter the weight and price for package 1"<<endl;
+    // cin >> w >> f;
+    // double cal1;
+    // cal1=f/w;
+    // cout<<"Enter the price for package 2"<<endl;
+    // cin >> w>> f;
+    // double cal2;
+    // cal2=f/w;
+    // if(cal1>cal2) {
+    //     cout<<"Package 1 is has better price"<<endl;
+    // }
+    // else if(cal1<cal2) {
+    //     cout<<"Package 2 is has better price"<<endl;
+    // }
+    // else {
+    //     cout<<"They are equal";}
+    //problem 15
+    // int a;
+    // cout << "Enter a three-digit number: ";
+    // cin >> a;
+    // int b;
+    // b=a/100;
+    // int c;
+    // c=a%10;
+    // if(b==c) {
+    //     cout<<a<<" is polindorme"<<endl;
+    // }
+    // else {
+    //     cout<<a<<" is not polindorme"<<endl;
+    // }
+    //problem 16
+// #include <cmath>
+//     int a, b;
+//     cin >> a >> b;
+//     int c;
+//     c= sqrt(pow(a,2) + pow(b,2));
+//     cout << c << endl;
+//     if (c<=10) {
+//         cout<<"Point "<<"("<<a <<","<<b<<")" << " is in the circle" <<endl;
+//     }
+//     else {
+//         cout<<"Point "<<"("<<a <<","<<b<<")" << "is not in the circle" <<endl;
+//     }
+//     return 0;
+//problem 17
+    float a;
+    cin>>a;
+
+
+
+
+
+
+}
